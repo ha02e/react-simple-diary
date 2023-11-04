c16ea9a39c423a3e9c685a1ff14866daa55e6ddc
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-11-04 23:58:18 +0900

    231104_배열을 이용해서 일기 리스트 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+//import React, { useState, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이하영",
+    content: "안녕",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "안녕2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "짱구",
+    content: "안녕3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
