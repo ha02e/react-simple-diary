b90b047adcfd9032f872465c745a0fa4a939c9dc
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-11-20 20:08:20 +0900

    231120_React.memo 실습2

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,34 +1,54 @@
 import React, { useState, useEffect } from "react";
 
-const Textview = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`Update :: Text : ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const Countview = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`Update :: Count : ${count}`);
+    console.log(`CounterA Update - count: ${count} `);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = React.memo(({ obj }) => {
+  useEffect(() => {
+    console.log(`CounterB Update - count: ${obj.count} `);
+  });
+  return <div>{obj.count}</div>;
+});
+
+const areEqual = (prevProps, nextProps) => {
+  return prevProps.obj.count === nextProps.obj.count;
+  // if (prevProps.obj.count === nextProps.obj.count) {
+  //   return true; //이전 프롭스와 현재 프롭스가 같다 => 리렌더링을 일으키지 않음
+  // }
+  // return false; //이전과 현재가 다르다 => 리렌더링이 일으킴
+};
+
+const MemorizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <Countview count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
       </div>
       <div>
-        <h2>text</h2>
-        <Textview text={text} />
-        <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        <h2>Counter B</h2>
+        <MemorizedCounterB obj={obj} />
+        <button
+          onClick={() =>
+            setObj({
+              count: obj.count,
+            })
+          }
+        >
+          B button
+        </button>
       </div>
     </div>
   );
