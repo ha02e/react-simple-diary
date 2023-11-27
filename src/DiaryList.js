de79c29dc6981e269f24687b6c46bf5de7346f3e
DESKTOP-0V1BAA9\HAYOUNG <ha02e@naver.com>
2023-11-27 22:43:53 +0900

    231127_Context로 데이터 스테이트 공급(DiaryList)

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import DiaryItem from "./DiaryItem.js";
+import { DiaryStateContext } from "./App.js";
 
-const DiaryList = ({ onEdit, onRemove, diaryList }) => {
+const DiaryList = ({ onEdit, onRemove }) => {
+  const diaryList = useContext(DiaryStateContext);
+
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
